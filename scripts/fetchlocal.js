bee088cb22c9709cfc0c1ca5211557c47622302b
Bobuxstation <arsyadyudhistira2@gmail.com>
2023-01-20 16:48:03 +0700

    launch options for unknown games

--- scripts/fetchlocal.js
+++ scripts/fetchlocal.js
@@ -4,36 +4,86 @@ const electron = require('electron');
 console.log(configDir);
 let jsonData = require(configDir + '/games.json');
 
+function launchELSE(dir, banner, name) {
+    document.getElementById('gameplay').onclick = function () {
+        document.getElementById("taskmgr").style.display = "block";
+
+        let taskname = document.createElement("p");
+        taskname.innerHTML = "<h2>" + name + "</h2><p>Select a way to open this game</p>";
+        taskname.id = name;
+
+        let execoptionbtn = document.createElement("button");
+        execoptionbtn.id = "endtask";
+        execoptionbtn.innerHTML = "Launch as an executable";
+        execoptionbtn.onclick = function () {
+            var proc = require('child_process').spawn(dir);
+            taskname.remove();
+    
+            let newtaskname = document.createElement("p");
+            newtaskname.innerHTML = "<h2>" + name + "</h2>" + "<button id='endtask'>Click To End Session</button>";
+            newtaskname.id = name
+            newtaskname.onclick = function () {
+                proc.kill('SIGINT');
+            }
+            newtaskname.className = "downloadinfo"
+            document.getElementById('tasks').appendChild(newtaskname);
+    
+            proc.on('exit', function (code, signal) {
+                newtaskname.remove()
+            });
+        }
+        taskname.appendChild(execoptionbtn);
+
+        let execoptionbtn1 = document.createElement("button");
+        execoptionbtn1.id = "endtask";
+        execoptionbtn1.innerHTML = "Launch as a web game";
+        execoptionbtn1.onclick = function () {
+            window.open(
+                "player.html?game=" + dir + "&banner=" + banner + "&name=" + name,
+                '_blank',
+                'icon= "./assets/logo.png",nodeIntegration=true,webviewTag=true, autoHideMenuBar= true, width= 1000, height= 600,'
+            );
+            taskname.remove()
+        }
+        taskname.appendChild(execoptionbtn1);
+
+        taskname.className = "downloadinfo notifytask";
+        document.getElementById('tasks').appendChild(taskname);
+    };
+}
+
 //function to launch html5 games
-function launchHTML(dir,banner,name) {
-    document.getElementById('gameplay').onclick = function(){
+function launchHTML(dir, banner, name) {
+    document.getElementById('gameplay').onclick = function () {
         window.open(
-            "player.html?game="+dir+"&banner="+banner+"&name="+name,
-             '_blank', 
-             'icon= "./assets/logo.png",nodeIntegration=true,webviewTag=true, autoHideMenuBar= true, width= 1000, height= 600,'
-            );};
+            "player.html?game=" + dir + "&banner=" + banner + "&name=" + name,
+            '_blank',
+            'icon= "./assets/logo.png",nodeIntegration=true,webviewTag=true, autoHideMenuBar= true, width= 1000, height= 600,'
+        );
+    };
 }
 
 //function to launch flash games
-function launchSWF(dir,banner,name) {
-    document.getElementById('gameplay').onclick = function(){
+function launchSWF(dir, banner, name) {
+    document.getElementById('gameplay').onclick = function () {
         window.open(
-            "flashplayer.html?game="+dir+"&banner="+banner+"&name="+name,
-             '_blank', 
-             'icon= "./assets/logo.png",nodeIntegration=true,webviewTag=true, autoHideMenuBar= true, width= 1000, height= 600,'
-            );};
+            "flashplayer.html?game=" + dir + "&banner=" + banner + "&name=" + name,
+            '_blank',
+            'icon= "./assets/logo.png",nodeIntegration=true,webviewTag=true, autoHideMenuBar= true, width= 1000, height= 600,'
+        );
+    };
 }
 
 //function to launch executable games
-function launchEXEC(dir,name) {
-    document.getElementById('gameplay').onclick = function(){
+function launchEXEC(dir, name) {
+    document.getElementById('gameplay').onclick = function () {
         document.getElementById("taskmgr").style.display = "block"
         var proc = require('child_process').spawn(dir);
 
         let taskname = document.createElement("p");
         taskname.innerHTML = "<h2>" + name + "</h2>" + "<button id='endtask'>Click To End Session</button>";
         taskname.id = name
-        taskname.onclick = function() {
+        taskname.onclick = function () {
             proc.kill('SIGINT');
         }
         taskname.className = "downloadinfo"
@@ -53,11 +103,13 @@ jsonData.items.forEach(items => {
         document.getElementById('gamename').textContent = items.name;
         document.getElementById('gamedev').textContent = items.developer;
         if (typeof items.type === "undefined" || items.type == "html5") {
-            launchHTML(items.dir,items.banner,items.name)
+            launchHTML(items.dir, items.banner, items.name)
         } else if (items.type == "executable") {
-            launchEXEC(items.dir,items.name)
+            launchEXEC(items.dir, items.name)
         } else if (items.type == "flash") {
-            launchSWF(items.dir,items.banner,items.name)
+            launchSWF(items.dir, items.banner, items.name)
+        } else {
+            launchELSE(items.dir, items.banner, items.name)
         }
         document.getElementById('gamedetails').style.display = "block";
         document.getElementById('body').style.backgroundImage = "url('" + items.banner + "')";
@@ -72,11 +124,13 @@ jsonData.items.forEach(items => {
     document.getElementById('gamename').textContent = items.name;
     document.getElementById('gamedev').textContent = items.developer;
     if (typeof items.type === "undefined" || items.type == "html5") {
-        launchHTML(items.dir,items.banner,items.name)
+        launchHTML(items.dir, items.banner, items.name)
     } else if (items.type == "executable") {
-        launchEXEC(items.dir,items.name)
+        launchEXEC(items.dir, items.name)
     } else if (items.type == "flash") {
-        launchSWF(items.dir,items.banner,items.name)
+        launchSWF(items.dir, items.banner, items.name)
+    } else {
+        launchELSE(items.dir, items.banner, items.name)
     }
     document.getElementById('gamedetails').style.display = "block";
     document.getElementById('body').style.backgroundImage = "url('" + items.banner + "')";
@@ -94,8 +148,8 @@ jsonData.items.forEach(items => {
 function taskmgr() {
     var x = document.getElementById("taskmgr");
     if (x.style.display === "none") {
-      x.style.display = "block";
+        x.style.display = "block";
     } else {
-      x.style.display = "none";
+        x.style.display = "none";
     }
-  }
+}
